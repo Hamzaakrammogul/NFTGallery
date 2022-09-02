dc21b65cd108eb04b776d8bbac4a7c137e2b3a27
Hamza Akram <101792969+Hamzaakrammogul@users.noreply.github.com>
2022-09-02 17:28:37 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { ethers } from "ethers";
 
 function App() {
+  const connect=async ()=>{
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  let res = await provider.send("eth_requestAccounts", []);
+  console.log(res);
+  }
+  const getData=()=>{
+    const options = {method: 'GET', headers: {Accept: 'application/json'}};
+
+    fetch('https://api.opensea.io/api/v1/collections?asset_owner=0x943590A42C27D08e3744202c4Ae5eD55c2dE240D&offset=0&limit=10', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+  };
+  const getAssetData=()=>{
+    const options = {method: 'GET'};
+
+fetch('https://api.opensea.io/api/v1/asset/0xa49a0e5eF83cF89Ac8aae182f22E6464B229eFC8/1/?include_orders=false', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={connect}>Connect To MetaMask</button>
+      <button onClick={getData}> Get Data</button>
+      <button onClick={getAssetData}>Get Asset Data</button>
     </div>
   );
 }
